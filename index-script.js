1f9957850928a95ebaf21acdbc37c265990b00a9
doodle0 <49020517+doodle0@users.noreply.github.com>
2023-06-30 15:40:14 +0900

    인트로 디자인 수정, 링크 페이지 추가

--- index-script.js
+++ index-script.js
@@ -3,8 +3,12 @@ function redirect(e) {
 }
 
 window.onload = (e) => {
-    document.getElementById("intro-btn").onclick = (e) => {
+    document.getElementById("btn-to-blog").onclick = (e) => {
         document.body.className += 'clicked';
-        setTimeout(redirect, 1000);
+        setTimeout((e) => {location.href = "/main"}, 1000);
+    }
+    document.getElementById("btn-to-links").onclick = (e) => {
+        document.body.className += 'clicked';
+        setTimeout((e) => {location.href = "/links"}, 1000);
     }
 }
---
+++ links.md
@@ -0,0 +1,11 @@
+---
+layout: page
+title: Links
+---
+
+## Projects
+
+[관악구 맞춤 월세 예측 (2023)](https://doodle0-gwanak-room-main-ocbqt1.streamlit.app/)
+
+[리얼북 검색 (2021-2023)](https://realbook.kro.kr/)
+
